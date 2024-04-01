fbe079479581b1bf0dd4880009e1c8400bf749ec
victorgarrido1 <vcgarr94@gmail.com>
2024-04-01 13:23:10 -0700

    checkpoint that works for now

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const path = require("path");
 const db = require("./config/connection");
+const { expressMiddleware } = require("@apollo/server/express4");
+
 const routes = require("./routes");
 
 //add the Apollo Server
@@ -8,35 +10,35 @@ const { ApolloServer } = require("apollo-server-express");
 const { typeDefs, resolvers } = require("./schemas");
 const { authMiddleware } = require("./utils/auth");
 
+const PORT = process.env.PORT || 3002;
 const app = express();
-const PORT = process.env.PORT || 3003;
-
 
 //apply the apollo middleware
 const startApolloServer = async () => {
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: authMiddleware,
-  });
-  console.log(`Using GraphQL at http://localhost:${PORT}${server.graphqPath}`);
-}
+  await server.start();
 
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+  app.use("/graphql", expressMiddleware(server));
+};
 
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+console.log(`Using GraphQL athttp://localhost:${PORT}/graphql}`);
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
-  
 }
-// app.use(express.static(path.join(__dirname, '../client/dist')));
-//     app.get('*', (req, res) => {
-//       res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-//     });
 
 app.use(routes);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+});
 
 db.once("open", () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
